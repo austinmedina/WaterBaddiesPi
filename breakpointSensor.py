91f9b33f745d5fa13e6d5b9cc51ac57e57c62d6a
austinmedina <austinmedina@comcast.net>
2025-02-18 16:56:47 -0700

    added IR Sensor class

--- breakpointSensor.py
+++ breakpointSensor.py
@@ -1,24 +1,21 @@
 from gpiozero import Button
 from signal import pause
+import time
 
 class IRSensor:
     def __init__(self, pin):
-        ir_sensor = Button(pin)
-        ir_sensor.when_pressed = on_ir_detected
-        ir_sensor.when_released = on_ir_cleared
+        self.ir_sensor = Button(pin)
+    
+    def is_object_detected(self):
+        if self.ir_sensor.value:
+            print("Object detected!")
+            return True
+        else:
+            print("No object detected!")
+            return False
         
-    def on_ir_detected():
-        return
-        print("IR Sensor Detected an Object!")
-    
-    def on_ir_cleared():
-        print("IR Sensor Cleared (No Object Detected)")
-    
-    def is_object_detected():
-    if ir_sensor.value:
-        print("Object detected!")
-        return True
-    else:
-        print("No object detected!")
-        return False
+if __name__ == "__main__":
+    ir = IRSensor(26)
+    while True:
+        print(ir.is_object_detected())
     
