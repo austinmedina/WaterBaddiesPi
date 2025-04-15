5e21e408727b97bc1ac47521e89fb0f331f17b25
austinmedina <austinmedina@comcast.net>
2025-04-14 21:18:22 -0700

    Added the new image processing algorithm

--- microscope_analysis.py
+++ microscope_analysis.py
@@ -1,16 +1,38 @@
-import matplotlib.pyplot as plt
-from skimage import io, color, filters, measure, morphology
+import math
+import numpy as np
+from skimage import io, color, filters, measure, morphology, draw
+
+
+def crop_image(image_path):
+    image = io.imread(image_path)
+
+    # Get image dimensions
+    height, width = image.shape[:2]
+    radius = min(width, height) // 2  # Make the circle fit within the image
+    center = (height // 2, width // 2)  # Center of the image
+
+    # Create a blank mask and draw a filled circle
+    mask = np.zeros((height, width), dtype=bool)
+    rr, cc = draw.disk(center, radius, shape=mask.shape)
+    mask[rr, cc] = True
+
+    # Apply the mask to the image (preserving color channels)
+    circular_image = np.zeros_like(image)
+    circular_image[mask] = image[mask]
+
+    return circular_image, radius
 
 
 def microplastic_concentration(image_path):
-    image = io.imread(image_path)
+    image, radius = crop_image(image_path)
     image = color.rgb2gray(image)
-    threshold = filters.threshold_otsu(image)
+    threshold = filters.threshold_yen(image)
+    # fig, ax = filters.try_all_threshold(image, figsize=(12,6), verbose=True)
 
     cleaned_image = morphology.closing(image >= threshold)
 
     # Label connected regions
-    labeled_image = measure.label(cleaned_image,background=0)
+    labeled_image = measure.label(cleaned_image, background=0)
     regions = measure.regionprops(labeled_image)
 
     # Count microplastic particles
@@ -18,30 +40,22 @@ def microplastic_concentration(image_path):
 
     # Known field of view (in mm²)
     # Adjust based on microscope calibration
-    obj_size = 1                        # in mm
-    obj_pixels = 62.9
+    obj_size = 1  # in mm
+    obj_pixels = 69
     scale_factor = obj_size / obj_pixels
 
     # Known field of view (in mm²)
-    field_of_view_area = (1280*scale_factor) * (720*scale_factor)
-    # field_of_view_area = 15*19
-    depth = 1                           # in mm
+    field_of_view_area = math.pi * (radius * scale_factor) ** 2
+    depth = 1  # in mm
     volume = field_of_view_area * depth / 1000
+
+    # Prevent falsely counting low-signal for particles
+    if num_particles >= 10:
+        num_particles = 0
 
     concentration = num_particles / volume
 
+    print(f'Particles counted: {num_particles} particles')
     print(f'Approximate concentration: {concentration:.2f} particles/mL')
-
-#     # Display
-#     fig, ax = plt.subplots(1, 2, figsize=(12, 6))
-# 
-#     ax[0].imshow(image)
-#     ax[0].axis("off")
-# 
-#     ax[1].imshow(labeled_image)
-#     ax[1].axis("off")
-# 
-#     plt.tight_layout()
-#     plt.show()
 
     return concentration
--- paperfluidic_analysis.py
+++ paperfluidic_analysis.py
@@ -2,6 +2,14 @@ import cv2
 import numpy as np
 from skimage import io, color
 
+
+def load_and_preprocess_image(image_path):
+    image = crop_image(image_path)
+    # Convert to Lab color space for better color comparison
+    lab_image = color.rgb2lab(image)
+    return lab_image
+
+
 def crop_image(image_path):
     image = io.imread(image_path)
 
@@ -25,54 +33,117 @@ def crop_image(image_path):
     cropped_image = image[y:y + h, x:x + w]
     return cropped_image
 
-def load_and_preprocess_image(image_path):
-    print("Preprocessing Image")
-    image = crop_image(image_path)
-    # Convert to Lab color space for better color comparison
-    lab_image = color.rgb2lab(image)
-    return lab_image
 
-
-def paperfluidic_concentration(reference_image_path, input_image_path,
+def paperfluidic_concentration(input_image_path,
                                region_radius=5):
-    color_changes = {"Nitrite": 0, "Lead": 0, "Cadmium": 0,
-                     "Nitrate": 0, "Phosphate": 0}
+    color_changes = {"Cadmium": 0, "Lead": 0, "Nitrate": 0,
+                     "Nitrite": 0, "Phosphate": 0}
     key_list = list(color_changes.keys())
     reservoir_coords = [(350, 450), (1020, 250),
                         (1410, 800), (1010, 1370), (340, 1170)]
 
-    reference_image = load_and_preprocess_image(reference_image_path)
     input_image = load_and_preprocess_image(input_image_path)
-    
-    print("Done preprocessing")
 
     for i, (x, y) in enumerate(reservoir_coords):
         # Extract small regions around the reservoir in both images
-        region_before = reference_image[y - region_radius:y + region_radius,
-                        x - region_radius:x + region_radius]
-        region_after = input_image[y - region_radius:y + region_radius,
-                       x - region_radius:x + region_radius]
+        region = input_image[y - region_radius:y + region_radius,
+                 x - region_radius:x + region_radius]
 
-        # Calculate the mean color
-        mean_color_before = np.mean(region_before, axis=(0, 1))
-        mean_color_after = np.mean(region_after, axis=(0, 1))
+        # Compute mean R, G, B values
+        mean_r = np.mean(region[:, :, 0])
+        mean_g = np.mean(region[:, :, 1])
+        mean_b = np.mean(region[:, :, 2])
 
-        # Compute the Euclidean distance
-        color_distance = np.linalg.norm(mean_color_after - mean_color_before)
+        color_value = 0.229*mean_r + 0.587*mean_g + 0.114*mean_b
 
-        color_changes[key_list[i]] = color_distance
-        print(color_distance)
+        if i == 0:
+            concentration = cadmium_concentration(color_value)
+        elif i == 1:
+            concentration = lead_concentration(color_value)
+        elif i == 2:
+            concentration = nitrate_concentration(color_value)
+        elif i == 3:
+            concentration = nitrite_concentration(color_value)
+        elif i == 4:
+            concentration = phosphate_concentration(color_value)
+
+        color_changes[key_list[i]] = concentration
 
     return color_changes
 
 
-# image_before_path = "./test_images/paperfluidic.jpg"
-# image_after_path = "./test_images/paperfluidic_test.jpg"
-# 
-# color_changes = paperfluidic_concentration(image_before_path, image_after_path)
-# 
-# print(color_changes)
-# 
-# #Display results
-# for reservoir, change in color_changes.items():
-#     print(f"{reservoir}: Color Change Distance = {change:.2f}")
+def cadmium_concentration(imagej):
+    if imagej >= 143.9:
+        return 0
+    elif 130.45 < imagej:
+        return (imagej - 144) / -1.35
+    elif 119.7 < imagej:
+        return (imagej - 132) / -0.119
+    elif 106.2 < imagej:
+        return (imagej - 123) / -0.0338
+    elif 98.4 < imagej:
+        return (imagej - 132) / -0.119
+    else:
+        return (imagej - 103) / (-4.13e-3)
+
+
+def lead_concentration(imagej):
+    if imagej >= 146.6:
+        return 0
+    elif 135.9 < imagej:
+        return (imagej - 147) / -1.07
+    elif 133.7 < imagej:
+        return (imagej - 136) / -0.0244
+    elif 126.8 < imagej:
+        return (imagej - 135) / -0.0173
+    elif 117.6 < imagej:
+        return (imagej - 136) / -0.0184
+    else:
+        return (imagej - 118) / (-7.43e-4)
+
+
+def nitrate_concentration(imagej):
+    if imagej >= 205.7:
+        return 0
+    elif 204 < imagej:
+        return (imagej - 206) / -0.068
+    elif 191.1 < imagej:
+        return (imagej - 217) / -0.516
+    elif 172.1 < imagej:
+        return (imagej - 210) / -0.38
+    elif 161 < imagej:
+        return (imagej - 175) / -0.0278
+    else:
+        return (imagej - 173) / -0.0234
+
+
+def nitrite_concentration(imagej):
+    if imagej >= 182.26:
+        return 0
+    elif 172.13 < imagej:
+        return (imagej - 182) / -10.1
+    elif 166.68 < imagej:
+        return (imagej - 173) / -1.36
+    elif 154.78 < imagej:
+        return (imagej - 179) / -2.38
+    elif 142.57 < imagej:
+        return (imagej - 167) / -1.22
+    elif 136.19 < imagej:
+        return (imagej - 149) / -3.19
+    else:
+        return (imagej - 147) / -0.273
+
+
+def phosphate_concentration(imagej):
+    if imagej >= 174.09:
+        return 0
+    elif 171.42 < imagej:
+        return (imagej - 174) / -0.0267
+    elif 160.98 < imagej:
+        return (imagej - 177) / -0.0522
+    elif 140.95 < imagej:
+        return (imagej - 191) / -0.1
+    elif 137.78 < imagej:
+        return (imagej - 144) / -(6.34e-3)
+    else:
+        return (imagej - 141) / (-3.01e-3)
